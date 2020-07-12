98bfa5b404adcd0cbe7186a11dde237fa74a2d7e
Djibba22 <johnwaynedinsmore@gmail.com>
2020-07-12 10:22:31 -0400

    adding signup, signin and home pages with material ui Fixes #17 && Fixes #18

--- chapter02/client/src/App.js
+++ chapter02/client/src/App.js
@@ -1,6 +1,39 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import Switcher from '@material-ui/core/Switch';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import Grid from '@material-ui/core/Grid';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import Header from './components/Header';
+import MainFeaturedPost from './components/MainFeaturedPost';
+import FeaturedPost from './components/FeaturedPost';
+import Main from './components/Main';
+import Sidebar from './components/Sidebar';
+import Footer from './components/Footer';
 import Home from "./pages/Home";
 import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import 'fontsource-roboto';
 import {
   BrowserRouter as Router,
   Switch,
@@ -9,29 +42,139 @@ import {
 } from "react-router-dom";
 
 import './App.css';
+import post1 from './blog-post.1.md';
+import post2 from './blog-post.2.md';
+import post3 from './blog-post.3.md';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    color: "white"
+  },
+  link: {
+    textDecoration: 'none'
+  },
+  mainGrid: {
+    marginTop: theme.spacing(3),
+  },
+}));
+
 
 function App() {
+
+  const classes = useStyles();
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleChange = (event) => {
+    setAuth(event.target.checked);
+  };
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleClick = () => {
+    //Todo Make this thing do something
+  };
+  const [value, setValue] = React.useState(0);
+
   return (
-    <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/how-it-works">How It Works</Link>
-          </li>
-        </ul>
-        <Switch>
-          <Route path="/how-it-works">
-            <About />
-          </Route>
-          <Route exact path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <>
+      <CssBaseline />
+      <Router>
+        {/* the temporary switch */}
+        <FormGroup>
+          <FormControlLabel
+            control={<Switcher checked={auth} onChange={handleChange} aria-label="login switch" />}
+            label={auth ? 'Logout' : 'Login'}
+          />
+        </FormGroup>
+        {/* this is the top bar to be included on every page */}
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              <a href="/" style={{textDecoration:"none"}}>KashTracker</a>
+          </Typography>
+            {auth && (
+              <div>
+                <IconButton
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleMenu}
+                  color="inherit"
+                >
+                  <AccountCircle />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  open={open}
+                  onClose={handleClose}
+                >
+                  <MenuItem onClick={handleClose}><Link to="/" className={classes.link}>Profile</Link></MenuItem>
+                  <MenuItem onClick={handleClose}><Link to="/how-it-works" className={classes.link}>My account</Link></MenuItem>
+                </Menu>
+              </div>
+            )}
+          </Toolbar>
+        </AppBar>
+        {  /* ToDo  put the menu bar */}
+        <Container maxWidth="lg">
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/signup">
+              <SignUp />
+            </Route>
+            <Route path="/signin">
+              <SignIn/>
+            </Route>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
+
+        </Container>
+        <BottomNavigation
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+          showLabels
+          className={classes.root}
+        >
+          <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+          <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+          <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+        </BottomNavigation>
+      </Router>
+    </>
   );
 }
 
